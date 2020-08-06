255fc8777a5dcb1b5f46b7c78b82ce73cbbdd29e
u_m11v2 <NEzhova@alfabank.ru>
2020-08-06 20:07:15 +0300

    add login_page in test

--- test_main_page.py
+++ test_main_page.py
@@ -14,5 +14,5 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
-    page = LoginPage(browser, browser.current_url)
-    page.should_be_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
